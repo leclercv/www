0a5526abb8945667e401727bd07e0ed9df4c8e6e
Leclercq Vincent <vinz.62580@hotmail.fr>
2015-05-19 03:28:13 +0200

    toast

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		 <center> Vincent Leclercq - Henri Dubois 
+		 <br>
+		 <h1>e-Testing system</h1>
+	</head>
+	<body>
+		<br>	
+		<center>
+		<div class="btn-group btn-group-lg" role="group" aria-label="...">
+		<button type="button" onclick="self.location.href='create.php'" class="btn btn-primary" onclick>Create a new test</button>
+		<button type="button" onclick="self.location.href='answer.php'" class="btn btn-primary"onclick>Answer a test</button>
+		</center>
+		</div>
+	</center>
+	</body>
+</html>
