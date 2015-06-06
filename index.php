1c5733f17a7dd67f5d6333a4b1b9c22055c0f073
Leclercq Vincent <vinz.62580@hotmail.fr>
2015-06-07 00:38:28 +0300

    cobite

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html>
 	<head>
 	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		 <center> Vincent Leclercq - Henri Dubois 
+	<link href="style/style.css" rel="stylesheet" media="all" type="text/css"> 
+
+		 <center> 
 		 <br>
 		 <h1>e-Testing system</h1>
 	</head>
@@ -14,6 +16,12 @@
 		<button type="button" onclick="self.location.href='answer.php'" class="btn btn-primary"onclick>Answer a test</button>
 		</center>
 		</div>
+
+		<img src="/style/akolegija.png" alt="Logo Akolegija">				
+		<img src="/style/lille1.png" alt="Logo Lille1">
+		<img src="lille1.png" alt="Logo Lille1">
+
+
 	</center>
 	</body>
 </html>
