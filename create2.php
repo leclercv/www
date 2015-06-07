434c956a2b92b514cb87390f155e33dbefe697da
Hendub <hendub62153@gmail.com>
2015-06-07 05:20:55 +0300

    mdr

--- create2.php
+++ create2.php
@@ -11,10 +11,17 @@ session_start();
 		 </center>
 	</head>
 <BODY>
-<h3>Your test has been succesfully created ! You will receive a mail soon with these informations : </h3>
 <?php
-echo '<h5>Token : '.$_SESSION['tok'].'</h5>';
-echo '<h5>The mail has been sent to : '.$_SESSION['to']."</h5></br>";
+	if(isset($_SESSION['tok']) && isset($_SESSION['to']))
+	{
+		echo '<h3>Your test has been succesfully created ! You will receive a mail soon with these informations : </h3>';
+		echo '<h5>Token : '.$_SESSION['tok'].'</h5>';
+		echo '<h5>The mail has been sent to : '.$_SESSION['to']."</h5></br>";
+	}
+	else
+	{
+	echo "<script type='text/javascript'>document.location.replace('create.php');</script>";
+	}
 ?>
 <center>
 <button type="button" class="btn btn-danger btn-lg" onclick="self.location.href='index.php'"onclick>Back to menu <span class="glyphicon glyphicon-home"></span></button>
