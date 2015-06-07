fdbf6bca08e4f0fd178059a8f44c1db7cb630f82
Leclercq Vincent <vinz.62580@hotmail.fr>
2015-06-07 21:18:38 +0300

    style answer2

--- answer2.php
+++ answer2.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
-$base = mysql_connect ('localhost', 'root', 'root');
-mysql_select_db ('etest', $base) ;
-mysql_query("SET NAMES UTF8"); 
+	session_start();
+	$base = mysql_connect ('localhost', 'root', 'root');
+	mysql_select_db ('etest', $base) ;
+	mysql_query("SET NAMES UTF8"); 
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,11 +14,10 @@ mysql_query("SET NAMES UTF8");
 	<body>
 
 		<div id="title">Answer an e-Test </div>
-		<div id="create">
+		<div id="answer">
 			<font color="white">
 			<?php
-			if (isset($_SESSION['token']) && isset($_SESSION['mail']))
-			{
+			if (isset($_SESSION['token']) && isset($_SESSION['mail'])){
 				$token = $_SESSION['token'];
 				$multrep = "";
 				$question = "";
@@ -31,85 +30,68 @@ mysql_query("SET NAMES UTF8");
 				$tabquest = explode(" | ", $question);
 				$tabansw = explode(" | ", $answer);
 				echo "<form method='post'>";
-				for($i = 0; $i < count($tabquest); $i++)
-				{
-					if($tabquest[$i]{0} == 1)
-					{
-						echo "<h1>".substr($tabquest[$i], 1)."</h1>";
+				for($i = 0; $i < count($tabquest); $i++){
+					echo "<h1>".substr($tabquest[$i], 1)."</h1>";
+
+					if($tabquest[$i]{0} == 1){
 						$tabanswcourant = explode(" _ ", $tabansw[$i]);
-							for($a = 0; $a < count($tabanswcourant); $a++)
-							{
-								echo '<input type="checkbox" name="cansw'.$a.'" value="'.$tabanswcourant[$a].'">'.$tabanswcourant[$a].'<br>';
+							for($a = 0; $a < count($tabanswcourant); $a++){
+								echo '   <input type="checkbox" name="cansw'.$a.'" value=" '.$tabanswcourant[$a].'">'.$tabanswcourant[$a].' 	   ';
 							}
 					}
-					if($tabquest[$i]{0} == 0)
-					{
-						echo "<h1>".substr($tabquest[$i], 1)."</h1>";
+					if($tabquest[$i]{0} == 0){
 						$tabanswcourant = explode(" _ ", $tabansw[$i]);
-							for($a = 0; $a < count($tabanswcourant); $a++)
-							{
-								if($a == 0)
-								{
-								echo '<input type="radio" name="ransw'.$i.'" value="'.$tabanswcourant[$a].'" checked>'.$tabanswcourant[$a].'<br>';
+							for($a = 0; $a < count($tabanswcourant); $a++){
+								if($a == 0){
+									echo '   <input type="radio" name="ransw'.$i.'" value=" '.$tabanswcourant[$a].'" checked>'.$tabanswcourant[$a].'  	  ';
 								}
-								else
-								{
-								echo '<input type="radio" name="ransw'.$i.'" value="'.$tabanswcourant[$a].'">'.$tabanswcourant[$a].'<br>';
+								else{
+									echo '   <input type="radio" name="ransw'.$i.'" value=" '.$tabanswcourant[$a].'">'.$tabanswcourant[$a].'    	';
 								}
 							}
 					}
 				}
-						echo '<br><input name ="valider2" class="btn btn-success btn-lg" type="submit" value="OK">';
-						echo "</form>";
-						if(isset($_POST['valider2'])){
-						for($z = 0 ; $z < count($tabquest) ; $z++)
-						{
+				echo '<br><br><input name ="valider2" class="btn btn-success btn-lg" type="submit" value="OK">';
+				echo "<button type='button' class='btn btn-danger btn-lg' onclick='self.location.href='index.php''onclick>Back to menu <span class='glyphicon glyphicon-home'></span></button>";
+
+				echo "</form>";
+				if(isset($_POST['valider2'])){
+					for($z = 0 ; $z < count($tabquest) ; $z++){
 						$hasansw = $hasansw.substr($tabquest[$z], 1)." : ";
 						$tabanswcourant = explode(" _ ", $tabansw[$z]);
-							if($tabquest[$z]{0} == 1)
-							{
-								$multrep = "";
-								for($y = 0; $y < count($tabanswcourant); $y++)
-								{
-									if(isset($_POST['cansw'.$y]))
-									{
-										$multrep = $multrep.$_POST['cansw'.$y]." / ";
-									}
+						if($tabquest[$z]{0} == 1){
+							$multrep = "";
+							for($y = 0; $y < count($tabanswcourant); $y++){
+								if(isset($_POST['cansw'.$y])){
+									$multrep = $multrep.$_POST['cansw'.$y]." / ";
 								}
-								$multrep = substr($multrep, 0, -2);
-								$hasansw = $hasansw.$multrep."\n";
 							}
-							if($tabquest[$z]{0} == 0)
-							{
-								$multrep = "";
-								for($x = 0; $x < count($tabanswcourant); $x++)
-								{
-									if(isset($_POST['ransw'.$x]))
-									{
-										$multrep = $multrep.$_POST['ransw'.$x]." ";
-									}
-								}
-								$hasansw = $hasansw.$multrep."\n";
-							}
+							$multrep = substr($multrep, 0, -2);
+							$hasansw = $hasansw.$multrep."\n";
 						}
-						$listmail = "";
-						$hasansw = "The user ".$_SESSION['mail']." "."has answered your test ! Here are his answers : \n".$hasansw;
-						$req = mysql_query("SELECT Mail FROM Form WHERE Token = '".$token."'") or exit(mysql_error());
-						list($listmail)=mysql_fetch_row($req); 
-						$tabmail = explode(" | ", $listmail);
-						mail($tabmail[0], 'Someone has answered your test !', $hasansw);
+						if($tabquest[$z]{0} == 0){
+							$multrep = "";
+							for($x = 0; $x < count($tabanswcourant); $x++){
+								if(isset($_POST['ransw'.$x])){
+									$multrep = $multrep.$_POST['ransw'.$x]." ";
+								}
+							}
+							$hasansw = $hasansw.$multrep."\n";
+						}
 					}
+					$listmail = "";
+					$hasansw = "The user ".$_SESSION['mail']." "."has answered your test ! Here are his answers : \n".$hasansw;
+					$req = mysql_query("SELECT Mail FROM Form WHERE Token = '".$token."'") or exit(mysql_error());
+					list($listmail)=mysql_fetch_row($req); 
+					$tabmail = explode(" | ", $listmail);
+					mail($tabmail[0], 'Someone has answered your test !', $hasansw);
 				}
-				else
-				{
+			}
+			else{
 				echo "<script type='text/javascript'>document.location.replace('answer.php');</script>";
-				}
-			?>
+			}
+		?>
 		</font>
-		</div>
-</br>
-<center>
-<button type="button" class="btn btn-danger btn-lg" onclick="self.location.href='index.php'"onclick>Back to menu <span class="glyphicon glyphicon-home"></span></button>
-</center>
+</div>
 </body>
 </html>
